0b381dd826331d84f6b8c850c35fd4951df01791
seaveyluo <seaveyluo@tencent.com>
2023-05-24 11:34:37 +0800

    chore: init

--- rspack.config.js
+++ rspack.config.js
@@ -1,26 +1,29 @@
 /**
-* @type {import('webpack').Configuration | import('@rspack/cli').Configuration}
-*/
+ * @type {import('webpack').Configuration | import('@rspack/cli').Configuration}
+ */
 module.exports = {
-    context: __dirname,
-    mode: "development",
-    devtool: false,
-    entry: {
-        main: "./src/index.tsx",
-    },
-    builtins: {
-        html: [
-            {
-                template: "./public/index.html"
-            }
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.webp$/i,
-                type: 'asset/resource'
-            }
-        ],
-    }
+  context: __dirname,
+  mode: "development",
+  devtool: false,
+  entry: {
+    main: "./src/index.tsx",
+  },
+  output: {
+    filename: "[name].[chunkhash][ext]"
+  },
+  builtins: {
+    html: [
+      {
+        template: "./public/index.html",
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.webp$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
